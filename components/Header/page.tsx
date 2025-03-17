31fd236c450962dc3ed327294401298cb7a4b134
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2025-03-17 10:24:24 +0530

    header done

--- components/Header/page.tsx
+++ components/Header/page.tsx
@@ -1,3 +1,4 @@
+import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import { Shirt, Sparkle } from 'lucide-react'
 import Link from 'next/link'
@@ -6,24 +7,34 @@ import React from 'react'
 const Header = () => {
   const isLoggedIn = false
   return (
-    <header className='flex flex-row justify-between items-center p-4 md:p-8'>
-        <div className='flex '>
-          <Shirt className=''/>
-          <h1 className='font-extrabold font-4xl'>
+    <header className='flex flex-row justify-between items-center p-4 md:p-8 mx-auto flex-1'>
+        <div className='flex gap-1 '>
+          <Shirt />
+          <h1 className='font-extrabold  font-4xl '>
         <Link href = "/"> Drip Check</Link>
         </h1>
         </div>
 
-        <div className='flex flex-row'>
-            <Sparkle className='animate-pulse'/>
-          AI-Powered system </div>
+        <div className='flex flex-row '>
+          <Badge className='p-2 md:p-3 overflow-hidden rounded-full bg-gradient-to-r from-rose-500 to-rose-400 text-white'>
+            <Sparkle className=' animate-pulse'/>
+          <h3>AI-Powered system </h3>
+          </Badge>
+        </div>
+        
         <div className='flex flex-row gap-2 md:gap-4 '>
+          {isLoggedIn ? (
+            <Button>
+            <Link href='sign-in'>Sign Out</Link> 
+          </Button>
+          ):
+          (
           <Button>
             <Link href='sign-in'>Sign In</Link>
           </Button>
-          <Button>
-            <Link href='sign-in'>Sign Out</Link> 
-          </Button>
+        )}
+          
+          
 
           
         </div>
