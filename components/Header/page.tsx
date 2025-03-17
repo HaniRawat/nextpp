35df54b57227cdbe760776bbb1361284a558fec9
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2025-03-17 20:06:40 +0530

    solved issue

--- components/Header/page.tsx
+++ components/Header/page.tsx
@@ -1,6 +1,6 @@
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
-import { SignedIn, SignedOut } from '@clerk/nextjs'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import { Shirt, Sparkle } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
@@ -19,21 +19,23 @@ const Header = () => {
         <div className='flex flex-row '>
           <Badge className='p-2 md:p-3 overflow-hidden rounded-full bg-gradient-to-r from-rose-500 to-rose-400 text-white'>
             <Sparkle className=' animate-pulse'/>
-          <h3>AI-Powered system </h3>
+          <h3>Ai Powered System</h3>
           </Badge>
         </div>
         
         <div className='flex flex-row gap-2 md:gap-4 '>
           <SignedIn>
-            <Button variant='ghost'>
-            <Link href='sign-in'>Sign Out</Link> 
-          </Button>
+            <div className='flex flex-row gap-2 md:gap-4 '>
+              <Button className='bg-gradient-to-r from-rose-500 to-rose-400 rounded-full'>
+                <Link href='/saved'>Saved</Link>
+              </Button>
+            <UserButton />
+            </div>
+            
           </SignedIn>
           
           <SignedOut>
-          <Button variant='ghost'>
-            <Link href='sign-in'>Sign In</Link>
-          </Button>
+            <SignInButton />
           </SignedOut>
 
         </div>
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp />
+  return <section className='flex justify-center items-center h-screen'>
+    <SignUp />
+  </section>
 }
