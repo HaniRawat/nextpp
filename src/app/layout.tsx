1985a5ff96daa3edf9a152e109ac2784f2bb7c5e
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2025-03-18 21:02:13 +0530

    improved directory structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Bebas_Neue } from "next/font/google";
 import "./globals.css";
-import Header from "../../components/Header/page";
-import Footer from "../../components/Footer/page";
+import Header from "../components/Header/page";
+import Footer from "../components/Footer/page"
 import { ClerkProvider } from "@clerk/nextjs";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import HeroSection from "@/components/home/page";
 
 export default function Home() {
   return <div>
-    
+
+    {/* apply gradient */}
+    <div>
     <HeroSection />
+    </div>
+    
   </div>
 }
